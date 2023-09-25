90084ab9e96715cd1ad6e88647386d85c0beca2d
oy_runchi <oy_runchi@qq.com>
2023-09-25 19:21:01 +0800

    Revert "暂存"
    This reverts commit aa68b40d8b648e3e518fab839304102424391ab0.

--- src/App.vue
+++ src/App.vue
@@ -4,47 +4,15 @@ import HelloWorld from './components/HelloWorld.vue';
 </script>
 
 <template>
-  SNOOPY
 
-
+  <header>
+    <RouterLink to="/calculateTest">calculateTest</RouterLink>
+  </header>
+  <div class="main">
+    <RouterView/>
+  </div>
 </template>
-<script setup lang="ts">
-import {G} from '@/game/gameGenerator';
-import {getCurrentInstance, reactive, ref} from 'vue';
-import {GOLD_CUT_MULTIPLE_NUMERATOR, SoldierGenerator} from '@/game/soldierGenerator';
 
-const internalInstance = getCurrentInstance();
-// 操作数据后更新视图
+<style scoped>
 
-
-const g = reactive(new G());
-g.SET_REF_SELF(g);
-
-const unlockSoldier = (soldier: SoldierGenerator) => {
-  g.unlockSoldier(soldier);
-};
-
-const toUploadAtk = (item: any) => {
-  item.UPGRADE_ATK();
-  internalInstance?.ctx?.$forceUpdate();
-};
-
-const toUploadSpd = (item: any) => {
-  item.UPGRADE_SPD();
-  internalInstance?.ctx?.$forceUpdate();
-};
-
-</script>
-<style scoped lang="less">
-ul {
-  li {
-    background-color: var(--paper-deep-yellow);
-    color: #000;
-    padding: 1rem;
-  }
-
-  li.active {
-    background-color: var(--paper-yellow);
-  }
-}
 </style>
