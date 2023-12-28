bb05d296585869ebf0f8952ea07e4dcdcdf83855
NightingaleM <1097591296@qq.com>
2023-12-28 10:31:03 +0800

    添加重置游戏按钮

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
+
+const reloadAll = () =>{
+  localStorage.clear()
+  location.reload()
+}
 </script>
 
 <template>
   <main>
     <RouterLink to="/calculateTest">calculateTest</RouterLink>
-
+    <button @click="reloadAll">重置所有内容</button>
     <!--    <TheWelcome />-->
   </main>
 </template>
